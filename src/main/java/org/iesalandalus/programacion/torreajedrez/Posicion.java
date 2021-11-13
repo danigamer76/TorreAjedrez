dbcb2b1500d79a3a8f63b6dddc820dfdd6731cfb
Daniel <Daniel@DESKTOP-FQNQR1B>
2021-11-13 14:50:55 +0100

    HashCode y Equals

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 	private int fila;
 	private char columna;
@@ -12,8 +14,7 @@ public class Posicion {
 			setFila(objPosicion.fila);
 			setColumna(objPosicion.columna);
 		}
-		
-		
+				
 	private void setFila(int fila) {
 		try {
 			if (fila >= 1 && fila <= 8) {
@@ -45,6 +46,22 @@ public class Posicion {
 	public char getColumna() {
 		return columna;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	
 	
 	
 }
