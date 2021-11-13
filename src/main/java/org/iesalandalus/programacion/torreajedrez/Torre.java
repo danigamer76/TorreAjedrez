c7ea43cabe1672400024b082baac87766226ec56
Daniel <Daniel@DESKTOP-FQNQR1B>
2021-11-13 18:29:09 +0100

    Añadido Metodo Mover

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,16 +1,18 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Torre{
 	private Color Color;
 	private Posicion Posicion;
-	
+
 	public Torre() {
 		setColor(Color.NEGRO);;
 		setPosicion(new Posicion(8, 'h'));
 	}
-	
+
 	public Torre(Color Color) {
-		if (Color == Color.NEGRO) {
+		if (Color.equals(Color.NEGRO)) {
 			setColor(Color.NEGRO);;
 			setPosicion(new Posicion(8, 'h'));
 		}else {
@@ -19,9 +21,9 @@ public class Torre{
 		}
 
 	}
-	
+
 	public Torre(Color Color, char columna) {
-		if (Color == Color.NEGRO) {
+		if (Color.equals(Color.NEGRO)) {
 			setColor(Color.NEGRO);;
 			setPosicion(new Posicion(8, columna));
 		}else {
@@ -29,9 +31,9 @@ public class Torre{
 			setPosicion(new Posicion(1, columna));
 		}
 	}
-	
+
 	private void setColor(Color color) {
-		if (Color == null) {
+		if (color.equals(null)) {
 			throw new NullPointerException("Error: No se puede definir ese color");
 		}else {
 			this.Color = color;
@@ -45,5 +47,83 @@ public class Torre{
 	}
 	public Posicion getPosicion() {
 		return Posicion;
+	}
+
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+		if(this.Color.equals(Color.BLANCO)) {
+			if(pasos < 0) {
+				throw new IllegalArgumentException("Pasos invalidos");
+			}else {
+				switch (direccion) {
+				case ARRIBA:
+					if (this.Posicion.getFila()+pasos > 8) {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila()+pasos,this.Posicion.getColumna()));
+					}
+					break;
+				case ABAJO:
+					if (this.Posicion.getFila()-pasos < 1) {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila()-pasos,this.Posicion.getColumna()));
+					}
+					break;
+				case IZQUIERDA:
+					if (this.Posicion.getColumna()-pasos < 'a') {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila(),(char) (this.Posicion.getColumna()-pasos)));
+					}
+					break;
+				case DERECHA:
+					if (this.Posicion.getFila()+pasos > 'h') {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila(),(char) (this.Posicion.getColumna()+pasos)));
+					}
+					break;
+				default:
+					throw new NullPointerException("Direccion Nula");
+				}
+			}
+		}else {
+			if(pasos < 0) {
+				throw new IllegalArgumentException("Pasos invalidos");
+			}else {
+				switch (direccion) {
+				case ABAJO:
+					if (this.Posicion.getFila()+pasos > 8) {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila()+pasos,this.Posicion.getColumna()));
+					}
+					break;
+				case ARRIBA:
+					if (this.Posicion.getFila()-pasos < 1) {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila()-pasos,this.Posicion.getColumna()));
+					}
+					break;
+				case DERECHA:
+					if (this.Posicion.getColumna()-pasos < 'a') {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila(),(char) (this.Posicion.getColumna()-pasos)));
+					}
+					break;
+				case IZQUIERDA:
+					if (this.Posicion.getFila()+pasos > 'h') {
+						throw new OperationNotSupportedException("Movimiento Invalido");
+					}else {
+						setPosicion(new Posicion(this.Posicion.getFila(),(char) (this.Posicion.getColumna()+pasos)));
+					}
+					break;
+				default:
+					throw new NullPointerException("Direccion Nula");
+				}
+			}
+		}
 	}
 }
