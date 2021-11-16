538a4338fc5bd94ca2184a97ce9c9c146ab4fb73
Daniel <Daniel@DESKTOP-FQNQR1B.Home>
2021-11-16 20:30:24 +0100

    Creados metodos Equals y Hasd Code en Torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 import javax.naming.OperationNotSupportedException;
 
 public class Torre{
@@ -175,4 +177,23 @@ public class Torre{
 			}
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Color, Posicion);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Torre other = (Torre) obj;
+		return Color == other.Color && Objects.equals(Posicion, other.Posicion);
+	}
+	
+	
 }
